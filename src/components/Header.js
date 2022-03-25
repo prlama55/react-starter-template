35e7e95c01761581c61e10192972ba7581553846
Padma Raj Lama <prlama@deerhold.org>
2022-03-25 10:59:50 +0545

    Flow setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,16 @@ const Header = ()=>{
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">React-Bootstrap starter</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item as={Link} to="/action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item as={Link} to="/action/3.4">Separated link</NavDropdown.Item>
+                        <Nav.Link as={Link} to="#home">Home</Nav.Link>
+                        <NavDropdown title="Users" id="collasible-nav-dropdown">
+                            <NavDropdown.Item as={Link} to="/users">User List</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/action/3.2">Create User</NavDropdown.Item>
+                            {/*<NavDropdown.Divider />*/}
+                            {/*<NavDropdown.Item as={Link} to="/action/3.4">Separated link</NavDropdown.Item>*/}
                         </NavDropdown>
                     </Nav>
                     <Form className="d-flex">
